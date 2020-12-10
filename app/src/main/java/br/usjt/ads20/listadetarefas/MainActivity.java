7110711d2195be6134a6aa638a4755b2ad1cd6f8
Caio05 <caiocastorideoliveira@gmail.com>
2020-12-09 21:45:11 -0300

    fodase

---
+++ app/src/main/java/br/usjt/ads20/listadetarefas/MainActivity.java
@@ -0,0 +1,72 @@
+package br.usjt.ads20.listadetarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtTarefa;
+    private ArrayList<Tarefa> tarefas;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtTarefa = findViewById(R.id.tarefa);
+        listView = findViewById(R.id.lista);
+        tarefas = TarefaDAO.recuperarTodas(this);
+        Log.d(Tarefa.TAG, "lista de tarefas " + tarefas);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TarefaDAO.apagarTarefa(tarefas.get(i).getId());
+                tarefas.remove(i);
+                setAdapter();
+                return false;
+            }
+        });
+
+        listView.setOnClickListener(new AdapterView.OnClickListener( ) {
+            @Override
+            public boolean onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                return false;
+            }
+        };
+    }
+
+        public void setAdapter(){
+        ArrayList<String> descricao = new ArrayList<>();
+        //ordena o arraylist de tarefas
+        Collections.sort(tarefas);
+
+        for(Tarefa tarefa: tarefas){
+            descricao.add(tarefa.getTarefa());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, descricao);
+        listView.setAdapter(adapter);
+    }
+
+    public void inserirTarefa(View view) {
+        String descricao = txtTarefa.getText().toString();
+        Tarefa tarefa = new Tarefa();
+        tarefa.setTarefa(descricao);
+        String id = TarefaDAO.inserir(tarefa);
+        tarefa.setId(id);
+        tarefas.add(tarefa);
+        txtTarefa.setText("");
+        setAdapter();
+    }
+}
